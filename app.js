e1426a267a7bd5d588111597419605108d2ac7f5
sejinpark <psj3205@gmail.com>
2019-07-28 14:40:39 +0900

    mocha, supertest로 유닛테스트

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -39,3 +42,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('./user.controller')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', controller.index);
+
+router.get('/:id', controller.show);
+
+router.delete('/:id', controller.destroy);
+
+router.post('/', controller.create);
 
 module.exports = router;
