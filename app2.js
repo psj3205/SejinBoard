89e88aa9770db815ac4442f8ce62a7108d2d4b6e
sejinpark <psj3205@gmail.com>
2019-08-13 18:11:30 +0900

    POST 요청 메소드 추출

--- app2.js
+++ app2.js
@@ -1,11 +1,19 @@
-var fs = require('fs');
 var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
 http.createServer((req, res) => {
-    fs.readFile('./views/index.html', (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-    });
+    if (req.method == 'GET') {
+        fs.readFile('./views/OtherPage.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else if (req.method == 'POST') {
+        req.on('data', (data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h1>' + data + '</h1>');
+        });
+    }
 }).listen(52273, () => {
     console.log('Server Running at http://127.0.0.1:52273');
 });
