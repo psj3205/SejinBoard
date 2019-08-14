580bb2bd8bc696c08ab9f33132c6f70dfacf03dc
sejinpark <psj3205@gmail.com>
2019-08-14 14:55:30 +0900

    express모듈을 이용한 서버 생성

--- app2.js
+++ app2.js
@@ -1,19 +1,14 @@
 var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
 
-http.createServer((req, res) => {
-    if (req.method == 'GET') {
-        fs.readFile('./views/OtherPage.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        });
-    } else if (req.method == 'POST') {
-        req.on('data', (data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('<h1>' + data + '</h1>');
-        });
-    }
-}).listen(52273, () => {
+var app = express();
+
+app.use((req, res) => {
+    var name = req.query.name;
+    var region = req.query.region;
+    res.send('<h1>' + name + '-' + region + '</h1>');
+})
+
+http.createServer(app).listen(52273, () => {
     console.log('Server Running at http://127.0.0.1:52273');
 });
