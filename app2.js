563612e0e289c7d84f7eeb722b3b09b2137b5f34
sejinpark <psj3205@gmail.com>
2019-08-16 14:12:49 +0900

    multer를 사용하여 파일 업로드 구현

--- app2.js
+++ app2.js
@@ -1,45 +1,56 @@
-var fs = require('fs');
 var http = require('http');
 var express = require('express');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var fs = require('fs');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'secret key',
+    resave: true,
+    saveUninitialized: true
+}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(__dirname + '/public'));
+app.use((req, res) => {
+    var output = {};
+    output.cookies = req.cookies;
+    output.session = req.session;
 
-app.get('/', (req, res) => {
-    if (req.cookies.auth) {
-        res.send('<h1>Login Success</h1>');
-    } else {
-        res.redirect('/login');
+    req.session.now = (new Date()).toUTCString();
+    res.send(output);
+})
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './views/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname);
+        console.log(Date.now());
     }
 });
+var upload = multer({ storage: storage });
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/login', (req, res) => {
-    fs.readFile('./views/login.html', (error, data) => {
+app.get('/upload', (req, res) => {
+    fs.readFile('./views/HTMLPage.html', (error, data) => {
         res.send(data.toString());
     });
 });
 
-app.post('/login', (req, res) => {
-    var login = req.body.login;
-    var password = req.body.password;
-
-    console.log(login, password);
+app.post('/upload', upload.any(), (req, res) => {
     console.log(req.body);
+    console.log(req.files);
 
-    if (login == 'rint' && password == '1234') {
-        res.cookie('auth', true);
-        res.redirect('/');
-    } else {
-        res.redirect('/login');
-    }
+    res.redirect('/upload');
 });
 
 http.createServer(app).listen(52273, () => {
